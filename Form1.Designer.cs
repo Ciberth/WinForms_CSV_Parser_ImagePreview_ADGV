1359713fb5741642660a5b6b1da2c2e6858a75a2
thomas.clauwaert <thomas.clauwaert@howest.be>
2021-08-05 17:00:40 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace CSV_Parser_ImagePreview_ADGV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.advancedDataGridView1 = new Zuby.ADGV.AdvancedDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // advancedDataGridView1
+            // 
+            this.advancedDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.advancedDataGridView1.FilterAndSortEnabled = true;
+            this.advancedDataGridView1.Location = new System.Drawing.Point(30, 97);
+            this.advancedDataGridView1.Name = "advancedDataGridView1";
+            this.advancedDataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.advancedDataGridView1.RowHeadersWidth = 51;
+            this.advancedDataGridView1.RowTemplate.Height = 29;
+            this.advancedDataGridView1.Size = new System.Drawing.Size(1012, 498);
+            this.advancedDataGridView1.TabIndex = 0;
+            this.advancedDataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.advancedDataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1383, 656);
+            this.Controls.Add(this.advancedDataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Zuby.ADGV.AdvancedDataGridView advancedDataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_Parser_ImagePreview_ADGV
+{
+    public partial class Form1 : Form
+    {
+        string csvPath = @"..\..\..\input.csv";
+
+        public List<csvItem> loadCSV(string csvFile)
+        {
+            var query = from l in File.ReadAllLines(csvFile)
+                        let data = l.Split(";")
+                        select new csvItem
+                        {
+                            Id = data[0],
+                            Name = data[1],
+                            Color = data[2],
+                            FotoPath = data[3],
+                            FotoThumb = (Bitmap)Bitmap.FromFile(data[3]).GetThumbnailImage(50, 50, null, new IntPtr())
+                        };
+            return query.ToList();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //set DataSource
+            advancedDataGridView1.RowTemplate.Height = 100;
+            advancedDataGridView1.DataSource = loadCSV(csvPath);
+            advancedDataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
+
+        }
+
+        private void advancedDataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class csvItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public String FotoPath { get; set; }
+        public Bitmap FotoThumb { get; set; }
+    }
+}
